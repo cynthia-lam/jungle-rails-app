ad5073f0d5a0211ee6e8a7a897560d76dc33b0c1
Cynthia <cynthia-lam@outlook.com>
2024-07-18 22:27:16 -0400

    Change to @product and add expect to look for specific error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,43 +15,54 @@ RSpec.describe Product, type: :model do
     end
 
     it 'must have a name' do
-      product = Product.new(
+      @category = Category.new
+      @product = Product.new(
         name: nil,
         price: 10,
         quantity: 1,
         category: @category
       )
-      expect(product).not_to be_valid
+      @product.save
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'must have a price' do
-      product = Product.new(
-        name: 'nil',
-        price: nil,
-        quantity: 1,
-        category: @category
-      )
-      expect(product).not_to be_valid
+      @category = Category.new
+      @product = Product.new(
+        name: "Test",
+        price_cents: nil,
+        quantity: "1",
+        category: @category)
+
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'must have a quantity' do
-      product = Product.new(
-        name: 'nil',
+      @category = Category.new
+      @product = Product.new(
+        name: 'name',
         price: 10,
         quantity: nil,
         category: @category
       )
-      expect(product).not_to be_valid
+      @product.save
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'must have a category' do
-      product = Product.new(
-        name: nil,
+      @category = Category.new
+      @product = Product.new(
+        name: 'name',
         price: 10,
         quantity: 1,
         category: nil
       )
-      expect(product).not_to be_valid
+      @product.save
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
