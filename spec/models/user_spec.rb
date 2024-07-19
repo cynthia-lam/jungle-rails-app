214d4c5c903c141bed00fbe12f60ecf3b0d09a76
Cynthia <cynthia-lam@outlook.com>
2024-07-18 23:07:12 -0400

    Remove unused params

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-<<<<<<< HEAD
   describe 'Validations' do
     it "must be created with a password and password_confirmation fields" do 
       user = User.new(
         email: 'test@test.com',
         password: 'password123',
-        password_confirmation: 'password123',
-        first_name: 'Test',
-        last_name: 'User'
+        password_confirmation: 'password123'
       )
       expect(user).to be_valid
     end
   end
-=======
-  pending "add some examples to (or delete) #{__FILE__}"
->>>>>>> ca06e73bbe2af471f0baaa6416a511f462d6dd59
 end
