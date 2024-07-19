f7ece7dd6947cab85847a8443399b1aa21a9c6ca
Cynthia <cynthia-lam@outlook.com>
2024-07-18 22:45:58 -0400

    Add user_spec first test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "must be created with a password and password_confirmation fields" do 
+      user = User.new(
+        email: 'test@test.com',
+        password: 'password123',
+        password_confirmation: 'password123',
+        first_name: 'Test',
+        last_name: 'User'
+      )
+      expect(user).to be_valid
+    end
+  end
+end
