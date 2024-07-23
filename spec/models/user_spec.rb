87d5b1426206b7cf2d0e9c952143778062b31831
Cynthia <cynthia-lam@outlook.com>
2024-07-23 11:32:50 -0400

    Add test for when email already taken

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,20 @@ RSpec.describe User, type: :model do
       )
       expect(user).not_to be_valid
     end
+
+    it 'is not valid if email exists (not case sensitive)' do
+      user_pass = User.new(
+        email: 'test@example.com',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+      user_fail = User.new(
+        email: 'TEST@EXAMPLE.COM',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+      expect(user_fail).not_to be_valid
+      # expect(user.errors.full_messages).to include("Email has already been taken")
+    end
   end
 end
